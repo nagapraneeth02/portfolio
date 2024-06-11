26ac8c90876248397ade0e63fe5847ddded5dd6b
Praneeth Cheela <praneeth.rishi02@gmail.com>
2024-06-11 05:59:14 -0400

    added about me

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,12 +1,36 @@
 import "../styles/about.css";
 import "../../public/praneeth.jpg";
+
 function AboutPage() {
   return (
     <div id="about">
       <h1>About</h1>
       <div className="about">
         <div>
-          <p>I'm a proffessional football player.</p>
+          <p>Hi, I'm Naga Praneeth Cheela.</p>
+          <p>
+            I'm a professional football player who loves the thrill of the game.
+            Football is not just a sport for me; it's a passion that drives my
+            competitive spirit.
+          </p>
+          <p>
+            When I'm not on the field, you can find me enjoying a game of
+            volleyball, which adds a great balance to my athletic life.
+          </p>
+          <p>
+            Beyond sports, I have a deep interest in coding. I enjoy solving
+            complex problems and creating innovative solutions through
+            programming.
+          </p>
+          <p>
+            I also love travelling and exploring new places. Every journey is an
+            opportunity to learn something new and meet interesting people from
+            different cultures.
+          </p>
+          <p>
+            My diverse interests in sports, technology, and travel keep me
+            constantly engaged and excited about life.
+          </p>
         </div>
       </div>
     </div>
